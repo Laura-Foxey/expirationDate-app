142f097271c8b84b4227547c373dbcb7365f6cc3
Laura.Foxey <roura_sasunaru@hotmail.com>
2023-01-03 00:25:24 +0000

    home style

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import FocusStatusBar from "../Components/FocusStatusBar.js"
 import ProductsList from "./ProductsList.js"
@@ -9,16 +9,37 @@ const Home = ({navigation}) => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <FocusStatusBar backgroundColor={"blue"} />
-            <TouchableOpacity onPress={() => navigation.navigate("ProductsList", {})}> 
-                <Text>Product List</Text> 
-            </TouchableOpacity>
-            <TouchableOpacity> 
-                <Text>Barcode List</Text> 
-            </TouchableOpacity>
+            <View style={styles.circleContainer}>
+                <TouchableOpacity style={styles.circle} onPress={() => navigation.navigate("ProductsList", {})}>
+                    <Text style={styles.circleText}>Product List</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.circle}>
+                    <Text style={styles.circleText}>Barcode List</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
 
 
-
 export default Home;
+
+const styles = StyleSheet.create({
+    circleContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    circle: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    circleText: {
+        textAlign: 'center',
+        color: 'white',
+    }
+})
